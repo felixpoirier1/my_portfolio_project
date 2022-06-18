d74183e291f8942c523876ebb170646c827361ac
felixpoirier1 <86891384+felixpoirier1@users.noreply.github.com>
2022-06-18 19:14:31 -0400

    commit

--- my_portfolio_project/settings.py
+++ my_portfolio_project/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     BASE_DIR / 'static'
-]
+]'''
 
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
